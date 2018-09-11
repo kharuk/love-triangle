d792ef8b2c78428f6ed73ca82af779569d0688ce
kharuk <frends.nastya@gmail.com>
2018-09-11 14:55:48 +0300

    task2

--- src/index.js
+++ src/index.js
@@ -3,5 +3,12 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+   let count = 0;
+   for (let i = 0; i <= preferences.length - 1; i++){
+       if ((preferences[preferences[preferences[i]-1]-1] == i+1) && (preferences[i] != i+1)){
+           count++;
+           console.log(preferences[preferences[preferences[i] - 1] - 1]);
+       }
+   }
+    return count/3;
 };
